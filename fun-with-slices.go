1c857a125e22a8ff65af5ca170e3c1d1ea6d57e8
Cormac Hogan <chogan@vmware.com>
2021-05-18 11:10:44 +0100

    Update fun-with-slices.go

--- fun-with-slices.go
+++ fun-with-slices.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 
+	var runes []rune
+
 	months := [...]string{
 		1: "January",
 		2: "February",
@@ -21,6 +23,7 @@ func main() {
 		12: "December"}
 
 	// create some slices from the array
+	
 	Q2 := months[4:7]
 	summer := months[6:9]
 
@@ -37,7 +40,7 @@ func main() {
 		}
 	}
 
-	// Slice beyond len is ok, but not cap
+	// Slice beyond len is ok, but not cap - slice is grown
 
 	endless_summer := summer[:5]
 	fmt.Printf("The new summer months are now %v\n\n", endless_summer)
@@ -50,12 +53,12 @@ func main() {
 		fmt.Printf("Summer is equal to endless summer - %t\n\n", result)
 	}
 
-	// Slice beyond len is ok, but not cap
+	// Slices do not support comparisons
 
 	new_summer := summer[:3]
 	fmt.Printf("The new summer months are now %v\n\n", new_summer)
 
-	// See if two slices are identical
+	// Special code to check if two slices are identical
 
 	if result := is_equal(summer, new_summer); result != true {
 		fmt.Printf("Summer is not equal to new summer - %t\n\n", result)
@@ -63,6 +66,33 @@ func main() {
 		fmt.Printf("Summer is equal to new summer - %t\n\n", result)
 	}
 
+	// The append function - print out the 11 runes in the string literal
+	// Append call could cause a reallocation, and assignment may not be reflected in new slice
+	// Thus common to assign result to the same slice variable, i.e. runes
+
+	for _, r := range "Hello,World" {
+		runes = append(runes, r)
+	}
+
+	fmt.Printf("Content as runes (Unicode code point) is %c\n", runes)
+	fmt.Printf("Content as integers is %d\n", runes)
+	fmt.Printf("Content as octals is %o\n", runes)
+	fmt.Printf("Content as hexadecimal is %X\n", runes)
+	fmt.Printf("Content as binary is %b\n", runes)
+	fmt.Printf("Content as quotes runes is %q\n", runes)
+
+	fmt.Printf("\nAppend to a slice\n")
+	runes = append(runes, 'X', 'X', 'X')
+	fmt.Printf("Content as quotes runes is %q\n", runes)
+
+	fmt.Printf("\nAppend the same slice to itself\n")
+	runes = append(runes, runes...)
+	fmt.Printf("Content as quotes runes is %q\n", runes)
+
+	fmt.Printf("\nRemove element from a slice\n")
+	mysl := []int{5, 6, 7, 8, 9}
+	fmt.Println(mysl)
+	fmt.Println(remove(mysl,2))
 }
 
 // Slices are not comparable - you need to perform the comparison yourself
@@ -77,4 +107,11 @@ func is_equal(x,y []string) bool {
 		}
 	}
 	return true
+}
+
+// Remove element from middle of slice, preserving the order
+
+func remove(slice []int, i int) []int {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
 }
